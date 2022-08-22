5eb5b93c14b49087985b9439028c175d0a91cd4e
Dilli Chalise <dillichalise@gmail.com>
2022-08-22 20:51:36 +0545

    About and Experience component added.

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -1,6 +1,8 @@
 import { Footer } from "./Footer";
 import { Header } from "./Header";
 import { Intro } from "./Intro";
+import { About } from "./About";
+import { Experience } from "./Experience";
 
 const Profile = () => {
   return (
@@ -8,6 +10,8 @@ const Profile = () => {
       <Header />
       <div className="page-content">
         <Intro />
+        <About />
+        <Experience />
       </div>
       <Footer />
     </>
